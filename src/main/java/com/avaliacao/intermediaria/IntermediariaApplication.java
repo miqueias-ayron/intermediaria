98ab3a9f30bbb1cf5e7a9cc5dbdfe36c584ed75c
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-09-21 16:05:40 -0300

    Initial commit

---
+++ src/main/java/com/avaliacao/intermediaria/IntermediariaApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.intermediaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermediariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntermediariaApplication.class, args);
+	}
+
+}
